05c675261afd330c13dcfc87cb12f03d3befabf9
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-06-11 19:13:18 -0500

    Adding Fragment

---
+++ app/src/main/java/com/adelphia2004/tomcat/HelloFragment.java
@@ -0,0 +1,26 @@
+package com.adelphia2004.tomcat;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by voltairea on 6/11/2017.
+ */
+
+public class HelloFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_hello, container, false);
+
+        //
+
+        return view;
+    }
+}
