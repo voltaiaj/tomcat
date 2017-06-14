fa6249638ff593288c2f8f91128001ffb19be0c9
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-06-13 19:12:31 -0500

    Adding FragmentManager

--- app/src/main/java/com/adelphia2004/tomcat/MainActivity.java
+++ app/src/main/java/com/adelphia2004/tomcat/MainActivity.java
@@ -1,5 +1,7 @@
 package com.adelphia2004.tomcat;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HelloFragment helloFragment = new HelloFragment();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.relativeLayout, helloFragment, "helloFragment");
+        transaction.commit();
     }
 }
