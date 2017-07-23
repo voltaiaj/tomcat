d8b19eeca075d100ef5e046c329f4dcd85bff4c9
Alexander Voltaire <Alexander.Voltaire@nov.com>
2017-07-22 23:14:03 -0500

    cheating

--- app/src/test/java/com/adelphia2004/tomcat/ExampleUnitTest.java
+++ app/src/test/java/com/adelphia2004/tomcat/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.adelphia2004.tomcat;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +16,20 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+        String data = "997f743f-ac3b-4e0d-83e4-5210c657a1092014-03-15 \n" +
+                "00:00:142014-03-15 00:06:55SUCCESS   50366436189285597836   997f743f-ac3b-4e0d-83e4-5210c657a1092014-03-15 \n" +
+                "00:00:142014-03-15 00:06:55SUCCESS   50366436189285597836";
+
+        String regex = "(.{36})([0-9]{4}-[0-9]{2}-[0-9]{2}\\s+[0-9]{2}:[0-9]{2}:[0-9]{2})([0-9]{4}-[0-9]{2}-[0-9]{2}\\s+[0-9]{2}:[0-9]{2}:[0-9]{2})([A-Z]\\w+)\\s+(\\d{10})(\\d{10})";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(data);
+
+        while(m.find()){
+            String result = m.group();
+        }
+
+
     }
 }
